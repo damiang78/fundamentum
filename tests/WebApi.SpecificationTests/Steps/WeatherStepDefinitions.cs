924ee028996b1ddcd8a6746983633a24fdb4e47b
Damian Gonsalves <damian.gonsalves@gmail.com>
2022-05-06 20:41:07 +0100

    Added specflow tests calling docker container

--- tests/WebApi.SpecificationTests/Steps/WeatherStepDefinitions.cs
+++ tests/WebApi.SpecificationTests/Steps/WeatherStepDefinitions.cs
@@ -1,4 +1,9 @@
+using FluentAssertions;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
 namespace WebApi.SpecificationTests.Steps
@@ -6,16 +11,47 @@ namespace WebApi.SpecificationTests.Steps
     [Binding]
     public class WeatherStepDefinitions
     {
-        [When(@"requesting the weather forecast")]
-        public void WhenRequestingTheWeatherForecast()
+        private readonly HttpClient _httpClient;
+        private readonly ScenarioContext _scenarioContext;
+
+        public WeatherStepDefinitions(HttpClient httpClient, ScenarioContext scenarioContext)
         {
-            throw new PendingStepException();
+            _httpClient = httpClient;
+            _scenarioContext = scenarioContext;
+        }
+
+        [When(@"requesting the weather forecast")]
+        public async Task WhenRequestingTheWeatherForecast()
+        {
+            var response = await _httpClient.GetAsync("weatherforecast");
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var forecast = JsonConvert.DeserializeObject<List<WeatherForecast>>(content);
+
+            _scenarioContext.Add("WeatherForecast", forecast);
         }
 
         [Then(@"we should get the weather forecast")]
         public void ThenWeShouldGetTheWeatherForecast()
         {
-            throw new PendingStepException();
+            var forecast = _scenarioContext.Get<List<WeatherForecast>>("WeatherForecast");
+
+            forecast.Should().NotBeNull();
+            forecast.Should().NotBeEmpty();
         }
+    }
+
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
     }
 }
